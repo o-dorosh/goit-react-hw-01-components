23238b6e9a60e6042884623a10354a32ab8ebf42
Olga Dorosh <odorosh28@gmail.com>
2021-06-29 19:31:02 +0300

    изменила стили для профиля, удалила лишний файл

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
-import { Profile } from '../Profile/Profile1';
+import { Profile } from '../Profile/Profile';
+import user from '../Profile/user.json';
 import './App.css';
 
 export function App() {
   return (
     <div>
-      <Profile />
+      <Profile
+        key={user.name}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 }
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import { GoLocation } from 'react-icons/go';
+import css from './Profile.module.css';
+
+export const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className="description">
+        <img
+          src={avatar}
+          alt="Аватар пользователя"
+          className={css.avatar}
+          width="150px"
+        />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>
+          <GoLocation className={css.icon} size="16" />
+          {location}
+        </p>
+      </div>
+
+      <ul class={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
